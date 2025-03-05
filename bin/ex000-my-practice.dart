1e3bc09d22b124209623d6ff8c6884fdac946271
Kim-kym <msgim725@gmail.com>
2025-03-05 18:05:43 +0900

    types practice

---
+++ bin/ex000-my-practice.dart
@@ -0,0 +1,17 @@
+// numbers() {
+//   int weight = 120;
+//   double height = 185.23;
+//
+//   print("weight: $weight, height: $height");
+//
+//   num weight2 = 120;
+//   num height2 = 190.244;
+//
+//   print("weight: $weight2, height: $height2");
+// }
+//
+// typeCheck()
+//
+// main () {
+//   numbers();
+// }
